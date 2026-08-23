69dfa37a3798321de1edd1d83e23403b8248448a
Huseyin Gunaydin <65340147+smitelagwar@users.noreply.github.com>
2026-08-23 14:11:34 +0300

    feat(dwg): export browser runtime policy

--- src/lib/dokumantasyon/dwg/index.ts
+++ src/lib/dokumantasyon/dwg/index.ts
@@ -1,6 +1,7 @@
 export * from "./converter";
 export * from "./diagnostics";
 export * from "./inspect";
+export * from "./runtime-policy";
 export * from "./signature";
 export * from "./snapshot";
 export * from "./types";
